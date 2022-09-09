ed256cd80ceb4c21aa7dfcd1d4e23be9c335b5c9
Codebox124 <rajcodes733@gmail.com>
2022-09-09 15:58:26 -0700

    done

---
+++ intro.js
@@ -0,0 +1,105 @@
+// // var age = 5;
+// // console.log(age + 1);
+// // console.log(++age);
+// // console.log(age **2 );
+// // console.log(--age);
+// // console.log(age);
+
+// //And = &&
+// //OR = ||
+// //Not = !
+// // = assignment operator
+// // == comparisim operator
+// // === strict comparism operator
+// // != not equal 
+// // !==
+// // +=
+
+// // console.log(10 == '10');
+// // console.log(10 === 10);
+// // console.log(10 !== '10')
+// /*
+// var age = 14;
+// console.log(age += 3);
+
+// var sum = 0;
+// console.log('inital value of sum : '+ sum);
+// var a  = 1;
+// var b = 2;
+// var sum = a + b;
+// console.log('final value : ' + sum);
+// */
+
+
+// //object
+// /*var _mySelf = {
+//     firstName: "Roqeeb",
+//     secondName: 'Raji',
+//     Age: 20,
+     
+// };
+// console.log(_mySelf.firstName);
+
+// // boolean
+// var $name = false;
+// if ($name){
+//     console.log($name);
+// }
+// else{
+//     console.log("ello")
+// }
+// */
+
+// // var score = 8;
+// // console.log("Mid-level skill: " + (0 < score && 10 > score));
+
+// // var timeRemaining = 0;
+// // var energy = 10;
+// // console.log("Game over : " +( timeRemaining == 0 || energy == 0))
+
+// //while loop
+// // let i = 10;
+// // do{
+// //     console.log(i);
+// //     i--;
+// // }
+// // while (i < 20);
+    
+// var num1 = 2;
+// var num2 =5;
+// var test1 = num1 % 2;
+// var test2 = num2 % 2;
+// var result1 = test1 == 0;
+// var result2 = test2 == 0;
+// console.log("Is" , num1 , "an even number?" , result1);
+// console.log("Is" , num2 , "an even number?" , result2)
+ 
+// //task 4
+// console.log(5 + 10);
+
+// var now = "Now in ";
+// var three = 3;
+// var d = "D!";
+// console.log(now + three + d);
+
+
+// var num1 = 3;
+// var num2 = 4;
+// var num3 = num1 + num2;
+// console.log(1 + 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
